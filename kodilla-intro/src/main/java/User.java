5071282115b26c754addb32de954412b74f98c4a
Arkadiusz Debczak <arkadiuszdebczak@wp.pl>
2020-01-26 20:37:11 +0100

    zadanko

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -2,10 +2,12 @@ public class User {
 
     String user;
     int age;
+    String hobby;
 
-    public User(String user, int age) {
+    public User(String user, int age, String hobby) {
         this.user = user;
         this.age = age;
+        this.hobby = hobby;
     }
 
     public String returnUser() {
@@ -18,15 +20,14 @@ public class User {
 
     public static void main(String[] args) {
 
+        User user1 = new User("Anna",  20, "pilka");
+        User user2 = new User("Betty", 33, "pilka");
+        User user3 = new User("Carl", 58, "pilka" );
+        User user4 = new User("David", 13, "pilka");
+        User user5 = new User("Eva", 18, "pilka");
+        User user6 = new User("Frankie", 45, "pilka");
 
-        User anna = new User("Anna", 20);
-        User betty = new User("Betty", 33);
-        User carl = new User("Carl", 58);
-        User david = new User("David", 13);
-        User eva = new User("Eva", 18);
-        User frankie = new User("Frankie", 45);
-
-        User[] users = {anna, betty, carl, david, eva, frankie};
+        User[] users = {user1, user2, user3, user4, user5, user6};
 
         int result = 0;//suma wieku
         for (int i = 0; i < users.length; i++) {
@@ -34,4 +35,12 @@ public class User {
         }
 
         int averageAge = result / users.length;//średnia wieku userów
+
         for (int i = 0; i < users.length; i++) {
+            if (users[i].age < averageAge) {
+                System.out.println(users[i].user);
+            }
+        }
+    }
+}
+
